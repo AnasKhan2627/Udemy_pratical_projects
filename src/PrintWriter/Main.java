9a6ad174f70934a03c57f72870230d2345bd4081
AnasKhan2627 <anaskhan_2627@gmail.com>
2020-07-16 23:48:50 +0530

    Project 2: PrintWriter completed.

--- src/PrintWriter/Main.java
+++ src/PrintWriter/Main.java
@@ -1,7 +1,11 @@
 package PrintWriter;
 
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println();
+        PrintWriter pw = new PrintWriter(System.out,true);
+        String text = "Project 2";
+        pw.println("This is "+text);
     }
 }
